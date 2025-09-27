6b14d6cf0a3fd1b8c5db26bfca451318c5d37bfd
xxxMichael <mayckolchavez27@gmail.com>
2025-09-27 15:49:19 -0500

    initialize Multi-company Sales System project with Maven and Spring Boot

---
+++ src/main/java/com/multicompany/sales_system/MultiCompanySalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.multicompany.sales_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiCompanySalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiCompanySalesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multicompany/sales_system/MultiCompanySalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicompany.sales_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCompanySalesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
