e16d42ef8e21e34ef2ca8fde4b71a034778d9c77
xxxMichael <mayckolchavez27@gmail.com>
2025-09-28 09:28:38 -0500

    agregar clases DTO PhotoRequestDTO, PhotoResponseDTO, ProductRequestDTO y ProductResponseDTO con validaciones

---
+++ src/main/java/com/multicompany/sales_system/dto/product/ProductResponseDTO.java
@@ -0,0 +1,28 @@
+package com.multicompany.sales_system.dto.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.multicompany.sales_system.dto.photo.PhotoResponseDTO;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponseDTO {
+    private Long idProducto;
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private String ubicacion;
+    private Boolean disponibilidad;
+    private String tipo;
+    private String estado;
+    private LocalDateTime fechaPublicacion;
+    private Long idVendedor;
+    private String nombreVendedor;
+    private List<PhotoResponseDTO> fotos;
+}
