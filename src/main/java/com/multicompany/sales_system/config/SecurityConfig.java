eaba971da0e0257f1616f56f1333737cd09af0c8
xxxMichael <mayckolchavez27@gmail.com>
2025-09-28 09:28:26 -0500

    formatear código en SecurityConfig.java para mejorar la legibilidad

--- src/main/java/com/multicompany/sales_system/config/SecurityConfig.java
+++ src/main/java/com/multicompany/sales_system/config/SecurityConfig.java
@@ -13,8 +13,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll())
-            .csrf(csrf -> csrf.disable());
-        
+                .csrf(csrf -> csrf.disable());
+
         return http.build();
     }
 }
